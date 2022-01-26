7eb28ee5a1247715093ce2df87e1e7d8248ada0b
Michael <soplomiha@gmail.com>
2022-01-26 21:02:26 +0300

    Adds a feature to download NASA APOD images

--- download_picture.py
+++ download_picture.py
@@ -1,23 +1,11 @@
 import os
+from dotenv import load_dotenv
 import requests
 from pathlib import Path
 from urllib.parse import urlparse
 
+
 PATH_IMAGES = "images"
-
-
-def fetch_spacex_last_launch() -> None:
-    url_all = "https://api.spacexdata.com/v4/launches"
-    response = requests.get(url_all)
-    response.raise_for_status()
-    launches = response.json()
-    for launch in launches[::-1]:
-        urls = launch["links"]["flickr"]["original"]
-        if urls:
-            break
-    for index, url in enumerate(urls):
-        filename = f"spaceX_flight{launch['flight_number']}_{index}.jpg"
-        download_image(url, filename)
 
 
 def download_image(url: str, filename: str) -> None:
@@ -36,7 +24,43 @@ def get_ext(url: str) -> str:
     return ext
 
 
+def fetch_spacex_last_launch() -> None:
+    url_all_launches = "https://api.spacexdata.com/v4/launches"
+    response = requests.get(url_all_launches)
+    response.raise_for_status()
+    launches = response.json()
+    for launch in launches[::-1]:
+        urls = launch["links"]["flickr"]["original"]
+        if urls:
+            break
+    for index, url in enumerate(urls):
+        filename = f"spaceX_flight{launch['flight_number']}_{index}.jpg"
+        download_image(url, filename)
+
+
+def fetch_nasa_apod(api_key: str, limit: int = 30) -> None:
+    url_apod = "https://api.nasa.gov/planetary/apod"
+    params = {
+        "api_key" : api_key,
+        "count" : limit, 
+    }
+
+    response = requests.get(url_apod, params=params)
+    response.raise_for_status()
+    descriptions = response.json()
+    for description in descriptions:
+        if description["media_type"] != "image":
+            continue
+        url = description["url"]
+        date_apod = description["date"]
+        ext = get_ext(url)
+        filename = f"nasa_apod_{date_apod}{ext}"
+        download_image(url, filename)
+
+
 if __name__ == "__main__":
+    load_dotenv()
+    api_key_nasa = os.getenv("API_KEY_NASA")
+
+    fetch_nasa_apod(api_key=api_key_nasa, limit=30)
     #fetch_spacex_last_launch()
-    url = "https://example.com/txt/hello%20world.txt?v=9#python"
-    print(get_ext(url))
