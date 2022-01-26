d90cd1d0535ccbdd96959b92a7069a32b74533f5
Michael <soplomiha@gmail.com>
2022-01-26 13:18:31 +0300

    Adds a function for downloading images

--- download_picture.py
+++ download_picture.py
@@ -3,16 +3,19 @@ from pathlib import Path
 
 
 PATH_IMAGES = "images"
-URL_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-FILENAME = "hubble.jpeg"
 
-if __name__ == "__main__":
-    headers = {
-        "User-Agent" : "download_picture"
-    }
-    response = requests.get(URL_IMAGE, headers=headers)
+
+def download_image(url: str, filename: str) -> None:
+    headers = {"User-Agent" : "download_picture"}
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
 
     Path(PATH_IMAGES).mkdir(parents=True, exist_ok=True)
-    with open(f"{PATH_IMAGES}/{FILENAME}", "wb") as file:
+    with open(f"{PATH_IMAGES}/{filename}", "wb") as file:
         file.write(response.content)
+
+
+if __name__ == "__main__":
+    url_image = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
+    filename = "hubble.jpeg"
+    download_image(url_image, filename)
