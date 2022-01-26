3c278d8c3164a39124aa63e45b23bc39c1f870cb
Michael <soplomiha@gmail.com>
2022-01-26 13:06:07 +0300

    Adds the creation of a catalog for images

--- download_picture.py
+++ download_picture.py
@@ -1,16 +1,18 @@
 import requests
+from pathlib import Path
 
 
+PATH_IMAGES = "images"
 URL_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
 FILENAME = "hubble.jpeg"
 
 if __name__ == "__main__":
-
     headers = {
         "User-Agent" : "download_picture"
     }
     response = requests.get(URL_IMAGE, headers=headers)
     response.raise_for_status()
 
-    with open(FILENAME, "wb") as file:
+    Path(PATH_IMAGES).mkdir(parents=True, exist_ok=True)
+    with open(f"{PATH_IMAGES}/{FILENAME}", "wb") as file:
         file.write(response.content)
